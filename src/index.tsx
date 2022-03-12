2e856ad00208d89e950da21ea8aa75e7121b8ab1
deepanshu <62979448+deepanshu44@users.noreply.github.com>
2022-03-12 16:39:53 +0530

    colors

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,25 @@ import { Button } from "react-bootstrap";
 import "./App.css"
 
 function App() {
-    const [color,setColor] = useState("primary")
+    const [color,setColor] = useState(["primary","danger","warning","success","light"])
     const [num,setNum] = useState(0)
     useEffect(()=>{
-	setTimeout(() => {
-	    setColor("danger")
-	    setNum(() => num+1)
-	}, 2000)
-    })
+	setTimeout(() => setColor((prevColor) => {
+	    let mprevColor = [...prevColor];
+	    mprevColor.unshift(mprevColor.pop())
+	    return mprevColor;
+	}), 400)
+    },[color])
     let Buttons2 =  ["danger","warning"].map((v,k) => (<Button variant={v}>Hello</Button>))
   return(
   <div> 
     some data
-    {Buttons2}<Button>hello</Button>
-    <Button variant={color}>Color</Button>
+    {Buttons2}<Button>hello</Button><br/><br/>
+    <Button variant={color[0]}>Color</Button>
+    <Button variant={color[1]}>Color</Button>
+    <Button variant={color[2]}>Color</Button>
+    <Button variant={color[3]}>Color</Button>
+    <Button variant={color[4]}>Color</Button>
     {num}
   </div>
   )
