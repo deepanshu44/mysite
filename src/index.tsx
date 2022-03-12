f893dcbb091bb6bb4193c98a3cd6d56a1e498c5a
deepanshu <62979448+deepanshu44@users.noreply.github.com>
2022-03-12 23:23:45 +0530

    color2

--- src/index.tsx
+++ src/index.tsx
@@ -4,28 +4,51 @@ import { Button } from "react-bootstrap";
 import "./App.css"
 
 function App() {
-    const [color,setColor] = useState(["primary","danger","warning","success","light"])
-    const [num,setNum] = useState(0)
-    useEffect(()=>{
-	setTimeout(() => setColor((prevColor) => {
-	    let mprevColor = [...prevColor];
-	    mprevColor.unshift(mprevColor.pop())
-	    return mprevColor;
-	}), 400)
-    },[color])
-    let Buttons2 =  ["danger","warning"].map((v,k) => (<Button variant={v}>Hello</Button>))
-  return(
-  <div> 
-    some data
-    {Buttons2}<Button>hello</Button><br/><br/>
-    <Button variant={color[0]}>Color</Button>
-    <Button variant={color[1]}>Color</Button>
-    <Button variant={color[2]}>Color</Button>
-    <Button variant={color[3]}>Color</Button>
-    <Button variant={color[4]}>Color</Button>
-    {num}
-  </div>
-  )
+    const [status,setStatus] = useState({val:0,color:""})
+    const [array,setArray] = useState([
+	{num:1,color:"red",active:false},
+	{num:2,color:"green",active:false},
+	{num:3,color:"blue",active:false},
+	{num:4,color:"orange",active:false},
+	{num:5,color:"purple",active:false}
+    ])
+    const test = (val:number,color:string) => {
+	setStatus(() => ({val,color}))
+	setArray((prev) => {
+	    let array = [...prev]
+	    array.map((z) => {
+		if (z.num===val) {
+		    z.active=true
+		    return z;
+		}else if (z.num===status.val) {
+		    z.active === false
+		    return z;
+		}
+	    });
+	    return array;
+	})
+    }
+    function styles(a,color) {
+	return{
+	    backgroundColor:a===status.val?color:"white",
+	    color:a===status.val?"white":color
+	};
+
+}
+
+    const buttons = array.map((value) => <button  key={value.num.toString()} onClick={()=>test(value.num,value.color)} style={styles(value.num,value.color)}>{value.color}</button>);
+    return(
+    <div className="top">
+      <div className="bar">
+	{buttons}
+	<div>a</div>
+      </div>
+      <div style={{backgroundColor:status.color}}>
+	{status.val}
+      </div>
+    </div>
+    );
+    
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
